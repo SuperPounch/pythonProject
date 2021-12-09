d60eef92c8e7a553c75674db4d4dc7a85a1e49fb
Lixiao <1240847431@qq.com>
2021-12-09 18:37:18 +0800

    add all Image Processing and Analysis

---
+++ Image Processing and Analysis/learn_python_opencv.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+# -*- coding:utf-8 -*-
+import cv2
+
+'''
+1、opencv 读图像主要调用函数如下：
+img = cv2.imread(文件名,[,参数])
+参数(1) cv2.IMREAD_UNCHANGED (图像不可变)
+参数(2) cv2.IMREAD_GRAYSCALE (灰度图像)
+参数(3) cv2.IMREAD_COLOR (读入彩色图像)
+参数(4) cv2.COLOR_BGR2RGB (图像通道BGR转成RGB)
+————————————————————————
+2、显示图像
+cv2.imshow(窗口名, 图像名)
+————————————————————————
+3、窗口等待
+cv2.waitKey(delay)
+键盘绑定函数，共一个参数，表示等待毫秒数，将等待特定的几毫秒，看键盘是否有输入，返回值为ASCII值。如果其参数为0，则表示无限期的等待键盘输入；参数>0表示等待delay毫秒；参数<0表示等待键盘单击。
+——————————————————————————
+4、删除所有窗口
+cv2.destroyAllWindows() 删除所有窗口
+cv2.destroyWindows() 删除指定的窗口
+————————————————————————————
+5、写入图片
+retval = cv2.imwrite(文件地址, 文件名)
+————————————————————————————
+
+版权声明：本文为CSDN博主「Eastmount」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/Eastmount/article/details/81748802
+'''
+'''
+Opencv像素处理
+1、读取像素
+'''
+
+img=cv2.imread('/Users/lx/Downloads/灰色1.jpeg',cv2.IMREAD_GRAYSCALE)
+
+'''
+cv2.imshow("gray",img)
+#无限期等待输入
+k=cv2.waitKey(0)
+#如果输入ESC退出
+if k==27:
+    cv2.destroyAllWindows()
+#在窗口界面，点击esc即为k=27请求退出，再去Python Console
+'''
+cv2.imwrite("/Users/lx/Downloads/gray2_2.jpeg",img)
--- Image Processing and Analysis/turnTocolor.py
+++ Image Processing and Analysis/turnTocolor.py
@@ -1,5 +1,106 @@
-import cv2
+#coding=utf-8
 
-im_gray = cv2.imread("H:/gdal/rs-data/result/ndvi.jpg", cv2.IMREAD_GRAYSCALE)
+'''
+ import cv2
+ im_gray = cv2.imread("/Users/lx/Downloads/灰色1.jpeg", cv2.IMREAD_GRAYSCALE)
+ im_color = cv2.applyColorMap(im_gray, cv2.COLORMAP_JET)
+ cv2.imwrite('/Users/lx/Downloads/灰色1.jpeg', im_color)
+'''
+
+"""
+import cv2
+img = cv2.imread('/Users/lx/Downloads/灰色1.jpg',2)
+im_gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
 im_color = cv2.applyColorMap(im_gray, cv2.COLORMAP_JET)
-cv2.imwrite('H:/gdal/rs-data/result/ndvi_color.jpg', im_color)
+cv2.imwrite('/Users/lx/Downloads/灰色1_1.jpg',im_color)
+"""
+
+
+'''
+import cv2
+import os.path
+import glob
+import numpy as np
+from PIL import Image
+
+def convertPNG(pngfile, outdir):
+	# 读取灰度图
+	im_depth = cv2.imread(pngfile)
+	# 转换成伪彩色（之前必须是8位图片）
+	# 这里有个alpha值，深度图转换伪彩色图的scale可以通过alpha的数值调整，我设置为1，感觉对比度大一些
+	im_color = cv2.applyColorMap(cv2.convertScaleAbs(im_depth, alpha=1), cv2.COLORMAP_JET)
+	# 转成png
+	im = Image.fromarray(im_color)
+	# 保存图片
+	im.save(os.path.join(outdir, os.path.basename(pngfile)))
+
+for pngfile in glob.glob("/Users/lx/Downloads/灰色1.jpg"):
+	convertPNG(pngfile, "/Users/lx/Downloads/灰色1.jpg")
+	'''
+
+
+
+
+'''
+__author__ = 'Administrator'
+import cv
+def Color(image):
+    w = image.width
+    h = image.height
+    size = (w,h)
+    iColor = cv.CreateImage(size,8,3)
+    for i in range(h):
+        for j in range(w):
+            r = GetR(image[i,j])
+            g = GetG(image[i,j])
+            b = GetB(image[i,j])
+            iColor[i,j] = (r,g,b)
+    return iColor
+
+def GetR(gray):
+    if gray < 127:
+        return 0
+    elif gray > 191:
+        return 255
+    else:
+        return (gray-127)*4-1
+
+
+def GetG(gray):
+    if gray < 64:
+        return 4*gray
+    elif gray > 191:
+        return 256-(gray-191)*4
+    else:
+        return 255
+
+def GetB(gray):
+    if gray < 64:
+        return 255
+    elif gray > 127:
+        return 0
+    else:
+        return 256-(gray-63)*4
+
+def FColor(image,array):
+    w = image.width
+    h = image.height
+    size = (w,h)
+    iColor = cv.CreateImage(size,8,3)
+    for i in range(h):
+        for j in range(w):
+            iColor[i,j] = array[int(image[i,j]/16)]
+    return iColor
+
+FCArray = [(0,51,0),(0,51,102),(51,51,102),(51,102,51),\
+            (51,51,153),(102,51,102),(153,153,0),(51,102,153),\
+            (153,102,51),(153,204,102),(204,153,102),(102,204,102),\
+            (153,204,153),(204,204,102),(204,255,204),(255,255,204)]
+image = cv.LoadImage('18.jpg',0)
+iColor = Color(image)
+iFColor = FColor(image,FCArray)
+cv.ShowImage('image',image)
+cv.ShowImage('iColor',iColor)
+cv.ShowImage('iFColor',iFColor)
+cv.WaitKey(0)
+'''
